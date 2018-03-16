2a101386e2acd576da51f7f8f7689a78976e5c91
William Bakst <William@DN0a1f6329.SUNet>
2018-03-16 10:33:53 -0700

    updated max word len

--- code/main.py
+++ code/main.py
@@ -35,6 +35,8 @@ from combined_model import CompleteModel
 from char_model import CharModel
 # from test import CompleteModel
 
+import shutil
+
 # TF_CPP_MIN_LOG_LEVEL=2
 
 models = {"baseline": QAModel, "AoA": QAoAModel, "BiDAF":BiDAFModel, "AnsPtr": AnsPtrModel, "complete":CompleteModel, "char":CharModel}
@@ -63,7 +65,7 @@ tf.app.flags.DEFINE_integer("batch_size", 100, "Batch size to use")
 tf.app.flags.DEFINE_integer("hidden_size", 200, "Size of the hidden states")
 tf.app.flags.DEFINE_integer("context_len", 400, "The maximum context length of your model")
 tf.app.flags.DEFINE_integer("question_len", 30, "The maximum question length of your model")
-tf.app.flags.DEFINE_integer("max_word_len", 20, "The maximum word length of your model (for character embeddings)")
+tf.app.flags.DEFINE_integer("max_word_len", 10, "The maximum word length of your model (for character embeddings)")
 tf.app.flags.DEFINE_integer("max_ans_len", 20, "Maximum span for any answer prediction")
 tf.app.flags.DEFINE_integer("embedding_size", 100, "Size of the pretrained word vectors. This needs to be one of the available GloVe dimensions: 50/100/200/300")
 
